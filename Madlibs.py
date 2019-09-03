3a7342fd91ce8b424b6d19ce7632465e48087079
Nyein Chan Aung <ninja.aung02@gmail.com>
2019-09-03 06:38:22 -0700

    Added Nescary Modules

---
+++ Madlibs.py
@@ -0,0 +1,5 @@
+from collections import defaultdict # Imports the ability to create writable instance variables
+from colorama import init, Fore, Back, Style # Imports the ability to color text and characters
+import random
+import re
+import sys
